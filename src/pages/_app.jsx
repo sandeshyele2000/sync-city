62c16a3a9acc290db722a7b8947b85066501bd6d
Sandesh <sandeshyele2000@gmail.com>
2024-06-30 17:23:45 +0530

    fixing font

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,13 @@
 import "./../styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import { StateProvider } from "../context/Context";
-import { QueryClient, QueryClientProvider } from 'react-query';
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClient}>
       <StateProvider>
         <Component {...pageProps} />
         <Toaster />
       </StateProvider>
-    </QueryClientProvider>
   );
 }
 
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginPage from './login'
+
+function index() {
+  return (
+    <LoginPage/>
+  )
+}
+
+export default index
