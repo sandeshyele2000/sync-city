ed882a5cc57952a4f62d34342dc4ae1736af76f1
Sandesh <sandeshyele2000@gmail.com>
2024-07-08 11:43:40 +0530

    minor ui changes and responsiveness

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,4 +2,4 @@ export const ROOM_LIMIT = 10;
 export const DEFAULT_PROFILE =
   "https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=";
 
-export const HOST_URL = "https://syncity.vercel.app/";
+export const HOST_URL = "https://syncity.vercel.app";
