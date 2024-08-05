e97de27facaaa0543fcdc178763c52963bf3e23a
Sandesh <sandeshyele2000@gmail.com>
2024-08-05 10:51:35 +0530

    Completed reply feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        'lh3.googleusercontent.com',
-        'i.ytimg.com',
-        'media.istockphoto.com'
-      ],
-    },
-}
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "i.ytimg.com",
+      "media.istockphoto.com",
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/socket.io/:path*',
+        destination: '/api/socketio/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
